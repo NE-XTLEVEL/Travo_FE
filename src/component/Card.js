7f140a6a0f388bb0cc815ceeca844518de6ecd2c
myeonghah <myeongha159357@gmail.com>
2025-04-08 12:59:21 +0900

    modify

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,35 +1,17 @@
 import './Card.css';
 import { ReactComponent as Option } from './assets/Card_option.svg';
 
-function typeNameChange(type) {
-  switch (type) {
-    case 'accommodation':
-      return '숙소';
-    case 'transport':
-      return '교통';
-    case 'restaurant':
-      return '식당';
-    case 'cafe':
-      return '카페';
-    case 'activity':
-      return '놀거리';
-    default:
-      '';
-      return type;
-  }
-}
-
 const Card = ({ item }) => {
   return (
     <div className="Card">
       <div className="CardContent">
-        <div className="CardType">{typeNameChange(item.type)}</div>
+        <div className="CardType">{item.category}</div>
         <div className="CardLowLine">
           <div className="CardName">{item.name}</div>
-          {item.isReservationNeeded && (
+          {item.url && (
             <div
               className="CardButton"
-              onClick={() => window.open(item.reservationUrl, '_blank')}
+              onClick={() => window.open(item.url, '_blank')}
             >
               예약하기
             </div>
