f72c1c20378a542c839c61a618376618bab74c9e
serin4103 <serin4103@gmail.com>
2025-05-02 19:30:15 +0900

    modify PlaceBin.css

--- src/component/PlaceBin.js
+++ src/component/PlaceBin.js
@@ -1,6 +1,6 @@
 import { useDroppable } from '@dnd-kit/core';
 import { GoTrash } from 'react-icons/go';
-import './PlaceBin.css'; //css 파일과 제대로 연결 안 됨
+import './PlaceBin.css';
 import classNames from 'classnames';
 
 export default function PlaceBin({ isActive }) {
@@ -12,23 +12,8 @@ export default function PlaceBin({ isActive }) {
     ActivePlaceBin: isActive,
   });
 
-  const style = {
-    width: isActive ? 80 : 60,
-    height: isActive ? 80 : 60,
-    backgroundColor: '#ffffff',
-    borderRadius: 70,
-    border: '2px solid #D9D9D9',
-    boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.1)',
-    position: 'fixed',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    bottom: 30,
-    right: 20,
-  };
-
   return (
-    <div className={classes} style={style} ref={setNodeRef}>
+    <div className={classes} ref={setNodeRef}>
       <GoTrash size="70%" color="#B0B0B0" />
     </div>
   );
