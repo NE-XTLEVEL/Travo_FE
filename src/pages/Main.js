3ef76832fcdb6d871e3164a48394fdbb0cbf60ef
myeonghah <myeongha159357@gmail.com>
2025-05-02 17:07:51 +0900

    main modify 1차

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,40 +1,62 @@
 import React from 'react';
-import Header from '../component/Header';
+import HeaderMain from '../component/HeaderMain';
 import './Main.css';
+import Main2 from './Main2';
 import { FiCalendar } from 'react-icons/fi';
-import { MdPeopleOutline } from 'react-icons/md';
+import { MdPeopleOutline, MdKeyboardArrowDown } from 'react-icons/md';
 import { LuSend } from 'react-icons/lu';
+import mainDescriptionImg from './assets/mainDescription.png';
+import mainPhonePCImg from './assets/mainPhonePCImg.png';
 
 const Main = () => {
   return (
-    <div className="Main">
-      <div className="MainHeader">
-        <Header />
-      </div>
-      <div style={{ width: '100vw' }}>
-        <div className="MainImage">어떤 여행을 떠나고 싶나요?</div>
-        <div className="MainContent">
-          <div className="MainOption">
-            <div className="OptionDate">
-              <FiCalendar />
-              <div>2025.04.09(수) ~ 2025.04.11(금)</div>
-            </div>
-            <div className="OptionPeople">
-              <MdPeopleOutline />
-              <div>2명</div>
-            </div>
+    <>
+      <div className="MainContainer">
+        <div className="Main">
+          <div className="MainHeader">
+            <HeaderMain />
           </div>
-          <div className="MainPrompt">
-            <input
-              className="MainPromptInput"
-              type="text"
-              placeholder="연인과 함께 북촌으로 봄 여행을 가고 싶어"
-            ></input>
-            <LuSend />
+          <div style={{ width: '100vw' }}>
+            <div className="MainImage">
+              <img
+                className="MainDescription wow fadeInLeft delay-05s animated"
+                src={mainDescriptionImg}
+                alt="MainDescription"
+              ></img>
+              <img
+                className="MainPhonePCImg wow fadeInUp delay-05s animated"
+                src={mainPhonePCImg}
+                alt="mainPhonePCImg"
+              ></img>
+            </div>
+            <div className="MainContent">
+              <div className="MainOption">
+                <div className="OptionDate">
+                  <FiCalendar size={'2.25vw'} />
+                  <div>2025.04.09(수) ~ 2025.04.11(금)</div>
+                </div>
+                <div className="OptionPeople">
+                  <MdPeopleOutline size={'3vw'} />
+                  <div>2명</div>
+                </div>
+              </div>
+              <div className="MainPrompt">
+                <input
+                  className="MainPromptInput"
+                  type="text"
+                  placeholder="연인과 함께 북촌으로 봄 여행을 가고 싶어"
+                ></input>
+                <LuSend size={'3vw'} stroke="#030045" strokeWidth={2} />
+              </div>
+              <div className="MainBelowButton">
+                <MdKeyboardArrowDown size={'3.75vw'} color="#B0B0B0" />
+              </div>
+            </div>
           </div>
         </div>
+        <Main2 />
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/pages/Main2.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <>
+      <div
+        className="Main2"
+        style={{
+          backgroundColor: 'yellowgreen',
+          width: '100vw',
+          height: '400vh',
+        }}
+      >
+        Main2입니다.
+      </div>
+    </>
+  );
+};
+
+export default Main;
